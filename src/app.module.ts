3f2f12409497dbcb17b8637a4cf8dc8f3de2e722
Majeed Shahbani <shahbani@xino.digital>
2022-12-31 19:30:39 +0000

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get<string>('MYSQL_DATABASE_HOST'),
+          port: configService.get<number>('MYSQL_DATABASE_PORT'),
+          username: configService.get<string>('MYSQL_DATABASE_USER'),
+          password: configService.get<string>('MYSQL_DATABASE_PASSWORD'),
+          database: configService.get<string>('MYSQL_DATABASE_NAME'),
+          synchronize: configService.get<string>('NODE_ENV') === 'development',
+          entities: [],
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
