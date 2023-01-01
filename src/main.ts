bf947629ba829c33d771c4499743073a12fd56bf
Majeed Shahbani <shahbani@xino.digital>
2023-01-01 21:05:14 +0000

    refactor(swagger): add conditional wrapper to setup only in dev env

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-import { VersioningType } from '@nestjs/common';
+import { INestApplication, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+function setupDocumentation(app: INestApplication) {
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,9 +17,7 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
-  const config = new DocumentBuilder().build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('documentation', app, document);
+  if (!isProduction) setupDocumentation(app);
   await app.listen(3000);
 }
 bootstrap();
