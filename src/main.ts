11018116f44f508f610797a7fd6435fad311616c
Majeed Shahbani <shahbani@xino.digital>
2023-01-01 14:08:28 +0000

    feat(main): add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
+    defaultVersion: '1',
   });
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
   await app.listen(3000);
 }
 bootstrap();
