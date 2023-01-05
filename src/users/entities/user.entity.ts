ad4e75b51654bf3aeee39010159d99ba3f34f540
Majeed Shahbani <shahbani@xino.digital>
2023-01-05 16:40:48 +0000

    refactor(users): add entiry base fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  fullName: string;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+  @Column()
+  isActive: boolean;
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
