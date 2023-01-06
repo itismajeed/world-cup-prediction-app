6681ce17de5b3da0bcde24712fb586a9e4d80b39
Majeed Shahbani <shahbani@xino.digital>
2023-01-06 07:51:39 +0000

    refactor(users): remove unnecessary sub module for auth

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { AuthController } from './auth/auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController, AuthController],
   providers: [UsersService],
   exports: [UsersService],
 })
