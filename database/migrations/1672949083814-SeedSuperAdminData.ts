c0e64bb04f3b6ad55eb4afc6508be489623c5cf6
Majeed Shahbani <shahbani@xino.digital>
2023-01-06 07:50:57 +0000

    refactor(user-migration): change name of encryption service

--- database/migrations/1672949083814-SeedSuperAdminData.ts
+++ database/migrations/1672949083814-SeedSuperAdminData.ts
@@ -1,4 +1,4 @@
-import { PasswordHasherService } from 'src/lib/password-hasher.service';
+import { PasswordEncryptionService } from 'src/lib/password-encryption.service';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { config } from 'dotenv';
 import { randomUUID } from 'crypto';
@@ -8,12 +8,12 @@ config();
 
 export class SeedSuperAdminData1672949083814 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const passwordHasherService = new PasswordHasherService();
+    const passwordEncryptionService = new PasswordEncryptionService();
     console.log({ password: process.env.ADMIN_PASSWORD });
     if (!process.env.ADMIN_PASSWORD) {
       throw new Error('ADMIN_PASSWORD env is required to run migrations.');
     }
-    const adminPasswordHash = await passwordHasherService.hash(
+    const adminPasswordHash = await passwordEncryptionService.hash(
       process.env.ADMIN_PASSWORD,
     );
     await queryRunner.query(
