3c6e0ad019875754f8111723129c4660795926a7
Majeed Shahbani <shahbani@xino.digital>
2023-01-07 18:06:14 +0000

    fix(lib): name of password encryption service token

--- src/lib/lib.module.ts
+++ src/lib/lib.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PASSWOORD_ENCRYPTION_SERVICE_TOKEN } from './interfaces/password-encryption-service.interface';
 import { PasswordEncryptionService } from './password-encryption.service';
 
 @Module({
   providers: [
     {
-      provide: 'IPasswordEncryptionService',
+      provide: PASSWOORD_ENCRYPTION_SERVICE_TOKEN,
       useClass: PasswordEncryptionService,
     },
   ],
+  exports: [PASSWOORD_ENCRYPTION_SERVICE_TOKEN],
 })
 export class LibModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { USERS_SERVICE_TOKEN } from './interfaces/users-service.interface';
+import { LibModule } from 'src/lib/lib.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
-  exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([User]), LibModule],
+  providers: [
+    {
+      provide: USERS_SERVICE_TOKEN,
+      useClass: UsersService,
+    },
+  ],
+  exports: [USERS_SERVICE_TOKEN],
 })
 export class UsersModule {}
