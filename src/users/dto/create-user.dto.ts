726f7c9891939c9586a0c02955f0724b62c3a3c5
Majeed Shahbani <shahbani@xino.digital>
2023-01-07 18:07:02 +0000

    refactor(users): add user dto props

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { Injectable } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+export class CreateUserDto {
+  @ApiProperty({ default: 'Some random name' })
+  fullName: string;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  password: string;
+}
