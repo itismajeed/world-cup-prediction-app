7c37f21e2443938c538f71e9a9d106afc1d02cab
Majeed Shahbani <shahbani@xino.digital>
2023-01-07 18:05:42 +0000

    fix(lib): name of password encryption service token

--- src/lib/interfaces/password-encryption-service.interface.ts
+++ src/lib/interfaces/password-encryption-service.interface.ts
@@ -2,3 +2,5 @@ export interface IPasswordEncryptionService {
   hash(password: string): Promise<string> | string;
   verify(password: string, hash: string): Promise<boolean> | boolean;
 }
+
+export const PASSWOORD_ENCRYPTION_SERVICE_TOKEN = 'IPasswordEncryptionService';
